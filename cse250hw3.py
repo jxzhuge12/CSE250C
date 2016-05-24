3efcb781a6937f4df55c8986fe70f161b7d6c711
Zhenghui Lai <laizh2015@gmail.com>
2016-05-24 15:10:28 -0700

    generate samples

--- cse250hw3.py
+++ cse250hw3.py
@@ -4,7 +4,7 @@ import random
 def sce1Proj(point):
     for i in xrange(len(point)):
         if np.sign(point[i]) >= 0: point[i] = 1
-        else: point[i] = 0
+        else: point[i] = -1
     return point
 
 def sce2Proj(point):
@@ -13,7 +13,7 @@ def sce2Proj(point):
 def pointGen(n, sigma, scenario = 0):
     y = np.zeros((n, 1))
     x = np.zeros((n, 4))
-    for int i in xrange(n):
+    for i in xrange(n):
         py = -1
         if random.random() >= 0.5: py = 1
         y[i] = py
@@ -21,6 +21,10 @@ def pointGen(n, sigma, scenario = 0):
         if py == -1: mu = -mu
         px = np.random.multivariate_normal(mu, np.eye(4) * sigma * sigma)
         if scenario == 1: px = sce1Proj(px)
-        else if scenario == 2: px = sce2Proj(px)
+        elif scenario == 2: px = sce2Proj(px)
         x[i,:] = px[:]
     return x, y        
+
+x, y = pointGen(10, 0.05, 1)
+for i in xrange(len(x)):
+    print"{}:{}".format(x[i], y[i])
